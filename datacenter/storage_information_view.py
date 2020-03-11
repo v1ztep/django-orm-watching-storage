24c4119017dfa77ae17bf896abdd5f3aea0e9431
v1ztep <v1ztep@gmail.com>
2020-03-11 21:04:15 +0300

    Подключил отображение на тех кто внутри

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,17 +2,26 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
+from django.utils.timezone import localtime
+from .models import get_duration, format_duration
 
 def storage_information_view(request):
-    # Программируем здесь
+    non_closed_visits = []
+    active_visits = Visit.objects.filter(leaved_at=None)
+    for visitor in active_visits:
+        visitor_name = visitor.passcard.owner_name
+        visitor_entered_time = localtime(visitor.entered_at)
+        duration_inside = format_duration(get_duration(visitor_entered_time))
 
-    non_closed_visits = [
-        {
-            "who_entered": "Richard Shaw",
-            "entered_at": "11-04-2018 25:34",
-            "duration": "25:03",
+        non_closed_visitor = {
+            "who_entered": visitor_name,
+            "entered_at": visitor_entered_time,
+            "duration": duration_inside,
         }
-    ]
+
+        non_closed_visits.append(non_closed_visitor)
+
+
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
