c3edfe1f0bcb613aaa95742c99c2ddcef29362ff
v1ztep <v1ztep@gmail.com>
2020-03-12 16:55:27 +0300

    внёс отображение всех посещений по карте

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,27 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from .models import get_duration, format_duration, is_visit_long
+from django.utils.timezone import localtime
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    this_passcard_visits = []
+    passcard = Passcard.objects.get(passcode=passcode)
+    passcard_visits = Visit.objects.filter(passcard=passcard)
+    for visit in passcard_visits:
+        visit_entered_time = localtime(visit.entered_at)
+        duration_inside = format_duration(get_duration(visit))
+        is_strange = is_visit_long(visit)
 
-    this_passcard_visits = [
-        {
-            "entered_at": "11-04-2018",
-            "duration": "25:03",
-            "is_strange": False
-        },
-    ]
+        visit = {
+            "entered_at": visit_entered_time,
+            "duration": duration_inside,
+            "is_strange": is_strange
+        }
+
+        this_passcard_visits.append(visit)
+
     context = {
         "passcard": passcard,
         "this_passcard_visits": this_passcard_visits
