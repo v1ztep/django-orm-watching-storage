7624b3d427859d3f2f51df37865b40d960ab7437
v1ztep <v1ztep@gmail.com>
2020-03-12 16:56:23 +0300

    Внёс изменения в имена и добавил проверку на подозрительность

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -3,26 +3,28 @@ from datacenter.models import Visit
 from django.shortcuts import render
 
 from django.utils.timezone import localtime
-from .models import get_duration, format_duration
+from .models import get_duration, format_duration, is_visit_long
+
 
 def storage_information_view(request):
     non_closed_visits = []
     active_visits = Visit.objects.filter(leaved_at=None)
-    for visitor in active_visits:
-        visitor_name = visitor.passcard.owner_name
-        visitor_entered_time = localtime(visitor.entered_at)
-        duration_inside = format_duration(get_duration(visitor_entered_time))
+    for visit in active_visits:
+        visit_name = visit.passcard.owner_name
+        visit_entered_time = localtime(visit.entered_at)
+        duration_inside = format_duration(get_duration(visit))
+        is_strange = is_visit_long(visit)
 
         non_closed_visitor = {
-            "who_entered": visitor_name,
-            "entered_at": visitor_entered_time,
+            "who_entered": visit_name,
+            "entered_at": visit_entered_time,
             "duration": duration_inside,
+            "is_strange": is_strange
         }
 
         non_closed_visits.append(non_closed_visitor)
 
-
     context = {
-        "non_closed_visits": non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
